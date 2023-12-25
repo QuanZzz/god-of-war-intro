9859f8a93fc9379fa26dc3a7d5bdcd1a1db9b5b8
QuanZzz <zhouquaneric@gmail.com>
2023-12-24 19:35:18 -0500

    .env uploaded

--- tina/config.js
+++ tina/config.js
@@ -11,9 +11,9 @@ export default defineConfig({
   branch,
 
   // Get this from tina.io
-  clientId: "5e724507-0bf3-4203-84c9-2e4eabf1a778",
+  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
   // Get this from tina.io
-  token: "550bf5ce00dc8bd18ea8b0f457361a532e58f45e",
+  token: process.env.TINA_TOKEN,
   build: {
     outputFolder: "admin",
     publicFolder: "public",
